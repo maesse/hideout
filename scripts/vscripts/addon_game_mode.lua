58caa0d2ecb49f261ca46a28f71294b2a06e332d
Mads <madslind@gmail.com>
2015-06-30 00:33:59 +0200

    Initial commit

---
+++ scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,149 @@
+-- Generated from template
+
+if CHideoutGameMode == nil then
+	CHideoutGameMode = class({})
+end
+
+require( "utility_functions" )
+
+function Precache( context )
+	--[[
+		Precache things we know we'll use.  Possible file types include (but not limited to):
+			PrecacheResource( "model", "*.vmdl", context )
+			PrecacheResource( "soundfile", "*.vsndevts", context )
+			PrecacheResource( "particle", "*.vpcf", context )
+			PrecacheResource( "particle_folder", "particles/folder", context )
+	]]
+end
+
+-- Create the game mode when we activate
+function Activate()
+	CHideoutGameMode:InitGameMode()
+end
+
+function CHideoutGameMode:InitGameMode()
+	print("Hideout is loaded.")
+	
+	GameRules:GetGameModeEntity().CHideoutGameMode = self
+	
+	self.countdownEnabled = false
+	
+--	Set team colors
+	self.m_TeamColors = {}
+	self.m_TeamColors[DOTA_TEAM_GOODGUYS] = { 61, 210, 150 }	--		Teal
+	self.m_TeamColors[DOTA_TEAM_BADGUYS]  = { 243, 201, 9 }		--		Yellow
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_1] = { 197, 77, 168 }	--      Pink
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_2] = { 255, 108, 0 }		--		Orange
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_3] = { 52, 85, 255 }		--		Blue
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_4] = { 101, 212, 19 }	--		Green
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_5] = { 129, 83, 54 }		--		Brown
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_6] = { 27, 192, 216 }	--		Cyan
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_7] = { 199, 228, 13 }	--		Olive
+	self.m_TeamColors[DOTA_TEAM_CUSTOM_8] = { 140, 42, 244 }	--		Purple
+	
+	for team = 0, (DOTA_TEAM_COUNT-1) do
+		color = self.m_TeamColors[ team ]
+		if color then
+			SetTeamCustomHealthbarColor( team, color[1], color[2], color[3] )
+		end
+	end
+	
+	self:GatherAndRegisterValidTeams()
+	
+	-- Show the ending scoreboard immediately
+	GameRules:SetCustomGameEndDelay( 0 )
+	GameRules:SetCustomVictoryMessageDuration( 10 )
+	GameRules:SetPreGameTime( 10 )
+	GameRules:GetGameModeEntity():SetLoseGoldOnDeath( false )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesOverride( true )
+	GameRules:GetGameModeEntity():SetTopBarTeamValuesVisible( false )
+	GameRules:SetHideKillMessageHeaders( true )
+	GameRules:SetUseUniversalShopMode( true )
+	
+	GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
+	print("Finished init.")
+end
+
+-- Evaluate the state of the game
+function CHideoutGameMode:OnThink()
+	-- Stop thinking if game is paused
+	if GameRules:IsGamePaused() == true then
+        return 1
+    end
+	
+	if self.countdownEnabled == true then
+		CountdownTimer()
+		if nCOUNTDOWNTIMER == 30 then
+			CustomGameEventManager:Send_ServerToAllClients( "timer_alert", {} )
+		end
+		if nCOUNTDOWNTIMER <= 0 then
+			--Check to see if there's a tie
+			if self.isGameTied == false then
+				GameRules:SetCustomVictoryMessage( self.m_VictoryMessages[self.leadingTeam] )
+				COverthrowGameMode:EndGame( self.leadingTeam )
+				self.countdownEnabled = false
+			else
+				self.TEAM_KILLS_TO_WIN = self.leadingTeamScore + 1
+				local broadcast_killcount = 
+				{
+					killcount = self.TEAM_KILLS_TO_WIN
+				}
+				CustomGameEventManager:Send_ServerToAllClients( "overtime_alert", broadcast_killcount )
+			end
+       	end
+	end
+	
+	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		--print( "Template addon script is running." )
+	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
+		return nil
+	end
+	
+	return 1
+end
+
+---------------------------------------------------------------------------
+-- Scan the map to see which teams have spawn points
+---------------------------------------------------------------------------
+function CHideoutGameMode:GatherAndRegisterValidTeams()
+--	print( "GatherValidTeams:" )
+
+	local foundTeams = {}
+	for _, playerStart in pairs( Entities:FindAllByClassname( "info_player_start_dota" ) ) do
+		foundTeams[  playerStart:GetTeam() ] = true
+	end
+
+	local numTeams = TableCount(foundTeams)
+	print( "GatherValidTeams - Found spawns for a total of " .. numTeams .. " teams" )
+	
+	local foundTeamsList = {}
+	for t, _ in pairs( foundTeams ) do
+		table.insert( foundTeamsList, t )
+	end
+
+	if numTeams == 0 then
+		print( "GatherValidTeams - NO team spawns detected, defaulting to GOOD/BAD" )
+		table.insert( foundTeamsList, DOTA_TEAM_GOODGUYS )
+		table.insert( foundTeamsList, DOTA_TEAM_BADGUYS )
+		numTeams = 2
+	end
+
+	local maxPlayersPerValidTeam = math.floor( 10 / numTeams )
+
+	self.m_GatheredShuffledTeams = ShuffledList( foundTeamsList )
+
+	print( "Final shuffled team list:" )
+	for _, team in pairs( self.m_GatheredShuffledTeams ) do
+		print( " - " .. team .. " ( " .. GetTeamName( team ) .. " )" )
+	end
+
+	print( "Setting up teams:" )
+	for team = 0, (DOTA_TEAM_COUNT-1) do
+		local maxPlayers = 0
+		if ( nil ~= TableFindKey( foundTeamsList, team ) ) then
+			maxPlayers = maxPlayersPerValidTeam
+		end
+		print( " - " .. team .. " ( " .. GetTeamName( team ) .. " ) -> max players = " .. tostring(maxPlayers) )
+		GameRules:SetCustomGameTeamMaxPlayers( team, maxPlayers )
+	end
+end
